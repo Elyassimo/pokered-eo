6a12ed0da08d56a14421841c62931c634fcc4845
waicalibre <69391221+waicalibre@users.noreply.github.com>
2020-08-10 03:13:25 +0000

    'NAME' -> 'NOMO' and centre in oak_speech2.asm

--- engine/movie/oak_speech/oak_speech2.asm
+++ engine/movie/oak_speech/oak_speech2.asm
@@ -165,7 +165,7 @@ DisplayIntroNameTextBox:
 	ld b, $a
 	ld c, $a
 	call TextBoxBorder
-	hlcoord 3, 0
+	hlcoord 4, 0
 	ld de, .namestring
 	call PlaceString
 	pop de
@@ -185,7 +185,7 @@ DisplayIntroNameTextBox:
 	jp HandleMenuInput
 
 .namestring
-	db "NAME@"
+	db "NOMO@"
 
 INCLUDE "data/player_names.asm"
 
